1488fc48e4c4266676744a8b76837421f7ec5d08
Hantonny Korrea <hantonny.20@gmail.com>
2020-04-21 17:33:48 -0400

    atualizacao

---
+++ administrator/components/com_joomlaupdate/restoration.php
@@ -0,0 +1,15 @@
+<?php
+defined('_AKEEBA_RESTORATION') or die('Restricted access');
+$restoration_setup = array(
+	'kickstart.security.password' => 'QobKe9rKoLwzpCWUnpMXSRboEGMkGmGn',
+	'kickstart.tuning.max_exec_time' => '5',
+	'kickstart.tuning.run_time_bias' => '75',
+	'kickstart.tuning.min_exec_time' => '0',
+	'kickstart.procengine' => 'direct',
+	'kickstart.setup.sourcefile' => '/var/www/html/site9ciagd/tmp/Joomla_3.9.18-Stable-Update_Package.zip',
+	'kickstart.setup.destdir' => 'C:\xampp\htdocs\site9ciagd',
+	'kickstart.setup.restoreperms' => '0',
+	'kickstart.setup.filetype' => 'zip',
+	'kickstart.setup.dryrun' => '0',
+	'kickstart.setup.renamefiles' => array(),
+	'kickstart.setup.postrenamefiles' => false);
